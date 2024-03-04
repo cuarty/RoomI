45c3afcfd01e9f7f0e408b5fdff0302c8ab17ac9
Nicolás Di Bartolo <dibartolo.fx@gmail.com>
2024-03-04 13:48:31 -0300

    actualizacion commit

---
+++ app.js
@@ -0,0 +1,55 @@
+function verInstructivo1() {
+    document.getElementById("b1b").style.display = "inline";
+    document.getElementById("b1a").style.display = "none";
+    document.getElementById("p1").style.display = "inline-block";
+
+}
+
+function ocultarInstructivo1() {
+    document.getElementById("b1b").style.display = "none";
+    document.getElementById("b1a").style.display = "inline";
+    document.getElementById("p1").style.display = "none";
+
+}
+
+function verInstructivo2() {
+    document.getElementById("b2b").style.display = "inline";
+    document.getElementById("b2a").style.display = "none";
+    document.getElementById("p2").style.display = "inline-block";
+
+}
+
+function ocultarInstructivo2() {
+    document.getElementById("b2b").style.display = "none";
+    document.getElementById("b2a").style.display = "inline";
+    document.getElementById("p2").style.display = "none";
+
+}
+
+function verInstructivo3() {
+    document.getElementById("b3b").style.display = "inline";
+    document.getElementById("b3a").style.display = "none";
+    document.getElementById("p3").style.display = "inline-block";
+
+}
+
+function ocultarInstructivo3() {
+    document.getElementById("b3b").style.display = "none";
+    document.getElementById("b3a").style.display = "inline";
+    document.getElementById("p3").style.display = "none";
+
+}
+
+function verInstructivo4() {
+    document.getElementById("b4b").style.display = "inline";
+    document.getElementById("b4a").style.display = "none";
+    document.getElementById("p4").style.display = "inline-block";
+
+}
+
+function ocultarInstructivo4() {
+    document.getElementById("b4b").style.display = "none";
+    document.getElementById("b4a").style.display = "inline";
+    document.getElementById("p4").style.display = "none";
+
+}
